96eb3978cc90f77a2845cc223b0ba20df7078e36
Yan Magdych <magdychyan@gmail.com>
2021-04-20 23:03:29 +0300

    Data layer designed. Added DbContext

---
+++ source/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext([NotNull] DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
