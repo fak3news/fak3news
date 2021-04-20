6a3d4e0c038a7cdeb63f3f70a4bb6c985bc92056
Yan Magdych <magdychyan@gmail.com>
2021-04-20 22:47:11 +0300

    Domain level designed

---
+++ source/Domain/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces
+{
+    public interface IRepository
+    {
+        public Task<T> Get<T>(int id) where T : DataBaseUnit, IModel;
+        public Task<IEnumerable<T>> GetAll<T>() where T : DataBaseUnit, IModel;
+        public Task<T> Create<T>(T entity) where T : DataBaseUnit, IModel;
+        public Task Update<T>(T entity) where T : DataBaseUnit, IModel;
+        public Task Delete<T>(T entity) where T : DataBaseUnit, IModel;
+    }
+}
