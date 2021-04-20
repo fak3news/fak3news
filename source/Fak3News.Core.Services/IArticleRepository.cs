fe075c88361141e13d551ea22d0e831af0d8ed94
qualterz <qualterz@tutamail.com>
2021-04-20 16:58:03 +0300

    Initial solution

---
+++ source/Fak3News.Core.Services/IArticleRepository.cs
@@ -0,0 +1,16 @@
+﻿using Fak3News.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fak3News.Core.Services
+{
+    public interface IArticleRepository
+    {
+        public Task CreateAsync(Article article);
+        public Task UpdateAsync(Article article);
+        public Task DeleteAsync(Guid id);
+        public Task<IEnumerable<Article>> GetAllAsync();
+        public Task<Article> GetAsync(Guid id);
+    }
+}
