4593361084e63edd0ff42cc6018f96ff76160330
qualterz <qualterz@tutamail.com>
2021-04-21 16:20:57 +0300

    Fix typo

--- source/Domain/Interfaces/IRepository.cs
+++ source/Domain/Interfaces/IRepository.cs
@@ -9,10 +9,10 @@ namespace Domain.Interfaces
 {
     public interface IRepository
     {
-        public Task<T> Get<T>(Guid id) where T : DataBaseUnit, IModel;
-        public Task<IEnumerable<T>> GetAll<T>() where T : DataBaseUnit, IModel;
-        public Task<T> Create<T>(T entity) where T : DataBaseUnit, IModel;
-        public Task Update<T>(T entity) where T : DataBaseUnit, IModel;
-        public Task Delete<T>(T entity) where T : DataBaseUnit, IModel;
+        public Task<T> Get<T>(Guid id) where T : DatabaseUnit, IModel;
+        public Task<IEnumerable<T>> GetAll<T>() where T : DatabaseUnit, IModel;
+        public Task<T> Create<T>(T entity) where T : DatabaseUnit, IModel;
+        public Task Update<T>(T entity) where T : DatabaseUnit, IModel;
+        public Task Delete<T>(T entity) where T : DatabaseUnit, IModel;
     }
 }
