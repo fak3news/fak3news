04b555e1222c2029208b83283bc242713a6091fc
unifrog <maden1337@gmail.com>
2021-04-23 21:05:04 +0300

    Add article list

--- source/Web/Pages/Admin.cshtml.cs
+++ source/Web/Pages/Admin.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Fak3News.Domain.Interfaces.Services;
+using Fak3News.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,18 @@ namespace Fak3News.Web.Pages
 {
     public class AdminModel : PageModel
     {
-        public void OnGet()
+        private readonly IArticleService articleService;
+        public List<Article> Articles { get; set; } = new List<Article>();
+
+        public AdminModel(IArticleService articleService)
         {
+            this.articleService = articleService;
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            Articles = (await articleService.GetAll()).ToList();
+            return Page();
         }
     }
 }
