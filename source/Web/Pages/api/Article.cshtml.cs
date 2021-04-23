1ac0ef565a3c70cc62d597dc6062ccefed8c7d91
Yan Magdych <magdychyan@gmail.com>
2021-04-23 14:39:23 +0300

    Article api added

---
+++ source/Web/Pages/api/Article.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fak3News.Domain.Interfaces.Services;
+using Fak3News.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace Fak3News.Web.Pages.api
+{
+    public class ArticleModel : PageModel
+    {
+        public ArticleModel(IArticleService service)
+        {
+            articleService = service;
+        }
+
+        private readonly IArticleService articleService;
+
+        public async Task<JsonResult> OnGetAsync(Guid? id = null)
+        {
+            if (id == null)
+                return new JsonResult(await articleService.GetAll());
+            else
+                return new JsonResult(await articleService.Get(id.Value));
+        }
+    }
+}
