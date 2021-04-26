ff848b6128e741f04b276e8c48460ab9c77eb3f1
Yan Magdych <magdychyan@gmail.com>
2021-04-26 15:49:37 +0300

    Added article page

---
+++ source/Web/Pages/Article.cshtml.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fak3News.Domain.Interfaces.Services;
+using Fak3News.Domain.Models;
+using Fak3News.Infrastructure.Services;
+using Fak3News.Web.WebServices;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fak3News.Web.Pages
+{
+    public class ArticleModel : PageModel
+    {
+        private readonly IArticleService articleService;
+
+        public IMarkdownService MarkdownService { get; set; }
+        public Article Article { get; set; }
+
+        public ArticleModel(IArticleService articleService, IMarkdownService markdownService)
+        {
+            this.articleService = articleService;
+            this.MarkdownService = markdownService;
+        }
+
+        public async Task<IActionResult> OnGet(Guid id)
+        {
+            Article = await articleService.Get(id);
+            return Page();
+        }
+    }
+}
