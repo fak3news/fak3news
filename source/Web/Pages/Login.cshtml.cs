a365ebdee4a1afb38275eefd5bf50128a6c219df
Yan Magdych <magdychyan@gmail.com>
2021-04-28 20:18:48 +0300

    Added Admin Authoriaztion

--- source/Web/Pages/Login.cshtml.cs
+++ source/Web/Pages/Login.cshtml.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Fak3News.Domain.Models;
+using Fak3News.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +13,36 @@ namespace Fak3News.Web.Pages
 {
     public class LoginModel : PageModel
     {
+        private readonly UserManager<Admin> userManager;
+        private readonly SignInManager<Admin> signInManager;
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public LoginModel(UserManager<Admin> userManager, SignInManager<Admin> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
         public void OnGet()
         {
         }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var result = await signInManager.PasswordSignInAsync(Email, Password, true, false);
+            if (result.Succeeded)
+                return RedirectToPage("Admin");
+            else
+                return Page();
+        }
+
     }
 }
