e9d9438cee0302979c98555f64b01ab05dc73b75
unifrog <maden1337@gmail.com>
2021-04-28 16:55:49 +0300

    Restructure admin functionality

--- source/Web/Pages/Admin.cshtml.cs
+++ source/Web/Pages/Admin.cshtml.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Fak3News.Domain.Interfaces.Services;
-using Fak3News.Domain.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,32 +9,9 @@ namespace Fak3News.Web.Pages
 {
     public class AdminModel : PageModel
     {
-        private readonly IArticleService articleService;
-        public List<Article> Articles { get; set; } = new List<Article>();
-
-        public AdminModel(IArticleService articleService)
+        public IActionResult OnGet()
         {
-            this.articleService = articleService;
-        }
-
-        public async Task<IActionResult> OnGet()
-        {
-            Articles = (await articleService.GetAll()).ToList();
-            return Page();
-        }
-        public async Task<IActionResult> OnPostDeleteAsync(Guid id)
-        {
-            var article = await articleService.Get(id);
-
-            if (article != null)
-                await articleService.Delete(article);
-
-            return new RedirectToPageResult("Admin");
-        }
-
-        public string IdToHtmlId(Guid id)
-        {
-            return "a" + id.ToString();
+            return RedirectToPage("Admin/Panel");
         }
     }
 }
